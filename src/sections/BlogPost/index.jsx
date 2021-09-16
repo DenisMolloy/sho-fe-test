cc6bb35efa3ef05766c480e297fc5cce6a127cba
Denis Molloy <denis@getmercutio.com>
2021-09-16 11:06:32 -0700

    added BlogIntroCard component

--- src/sections/BlogPost/index.jsx
+++ src/sections/BlogPost/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import RichText from 'frontend-ui/RichText'
+import Banner from 'Components/Banner'
+import BlogIntroCard from 'Components/BlogIntroCard'
+import Container from 'Components/Container'
+import Divider from 'Components/Divider'
+import exampleRichText from './exampleRichText'
 import './styles.css'
 
 const BlogPost = (blogPost = {}) => {
   console.log("This is blogPost:", blogPost)
+  const { heroImage, content }= blogPost
+ 
   return (
-    <div>
-      <h1>This is a blog post</h1>
-    </div>
+    <Container m={5}>
+      <Banner src={heroImage} />
+      <BlogIntroCard {...blogPost} />
+      <Divider/>
+      <RichText source={content || exampleRichText} />
+    </Container>
   )
 }
-export default BlogPost
+export default BlogPost;
