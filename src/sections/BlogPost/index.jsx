9b2a45e8b0a7dcb458b2d154dfc6c1d63cf8f45c
Denis Molloy <denis@getmercutio.com>
2021-09-21 15:52:07 -0700

    BlogIntroCard styled.  Updated BlogPost story to reflect how blogPost prop is passed in Shogun FE.

--- src/sections/BlogPost/index.jsx
+++ src/sections/BlogPost/index.jsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import RichText from 'frontend-ui/RichText'
-import ResponsiveImage from 'frontend-ui/ResponsiveImage'
 import BlogIntroCard from 'Components/BlogIntroCard'
 import Container from 'Components/Container'
-import Divider from 'Components/Divider'
 import './styles.css'
 
-const BlogPost = (blogPost = {}) => {
-  console.log("this is blogPost: ", blogPost)
-  if(blogPost && Object.keys(blogPost).length === 0) return "No props passed"
+const BlogPost = ({ blogPost }) => {
+  console.log('this is blogPost: ', blogPost)
+  if (blogPost && Object.keys(blogPost).length === 0) return 'No props passed'
 
-  const { heroImage, content } = blogPost
-
-  if(!heroImage) return null
+  const { content } = blogPost
 
   return (
-    <Container m={5}>
-      <ResponsiveImage src={heroImage || ""} />
+    <Container>
       <BlogIntroCard {...blogPost} />
-      <Divider/>
       <RichText source={content || []} />
     </Container>
   )
 }
-export default BlogPost;
+export default BlogPost
